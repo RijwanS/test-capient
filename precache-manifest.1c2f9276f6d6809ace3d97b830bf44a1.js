682bf1fd98e6fa75dc33ce94f2cba4717890b7e8
RijwanS <67869399+RijwanS@users.noreply.github.com>
2020-09-03 01:46:00 +0530

    Updates

---
+++ precache-manifest.1c2f9276f6d6809ace3d97b830bf44a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7789bc52109763eb833be745cf35600",
+    "url": "/test-capient/index.html"
+  },
+  {
+    "revision": "4053e55f3910b205a1a2",
+    "url": "/test-capient/static/css/main.108454b5.chunk.css"
+  },
+  {
+    "revision": "a04559ff5931e24ed1fa",
+    "url": "/test-capient/static/js/2.64d6b3b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-capient/static/js/2.64d6b3b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4053e55f3910b205a1a2",
+    "url": "/test-capient/static/js/main.6f6d8ebf.chunk.js"
+  },
+  {
+    "revision": "4986aa5c3a56a9e66af7",
+    "url": "/test-capient/static/js/runtime-main.54b4ab02.js"
+  }
+]);
